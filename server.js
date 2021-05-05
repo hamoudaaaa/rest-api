4fdb746201087fdba466f9dc783cc8ad3535bb96
Asma <ahmedbenmiled98@gmail.com>
2021-05-05 09:07:59 +0100

    first api check

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/connectDB");
+const router = require("./Route/user");
+
+connectDB();
+app.use(express.json());
+
+app.use("/rest", router);
+const port = 5000;
+app.listen(port, () => {
+    console.log(`le port is running ${port}`);
+});
